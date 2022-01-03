08e542a7715248667699c672ca521ec415fb05e6
samrkinn <samrkinn@gmail.com>
2022-01-02 21:19:16 -0800

    added some documentation

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 import uvicorn
 
-# gunicorn app.app:app --bind 0.0.0.0:5000 -k uvicorn.workers.UvicornWorker
 if __name__ == '__main__':
     uvicorn.run('ddbapi.app.app:app',
                 host='0.0.0.0',
