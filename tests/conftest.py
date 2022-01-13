4880946efa532e570c9972e54d5d28fee073bed4
Russel Torres <russtorres@gmail.com>
2022-01-13 11:18:37 -0800

    tests: switch to new multiprocessing-based server for client test, introduce envvars, add utility fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,71 +1,94 @@
-import pytest
-from subprocess import Popen
-import time
+import contextlib
+import copy
+import os
+import posixpath
 
 import pymongo
-import uvicorn
+import pytest
 
-from api.ddbapi.app.app import app
-from test_data import acquisition_data
+from .dummy_ddbapi_server import run_dummy_server
+from .test_data import acquisition_data
 
-mongoclient = pymongo.MongoClient('mongodb://localhost:27017')
-db = mongoclient['testdb']
+mongo_uri = os.getenv(
+        "DISPIMDB_TEST_MONGODB_URI",
+        "mongodb://localhost:27017")
+mongo_db = os.getenv(
+        "DISPIMDB_TEST_MONGODB_DB",
+        "dispimdb_test_db")
+ddbapi_test_url = os.getenv(
+    "DISPIMDB_CLIENT_TEST_DISPIMDB_URL",
+    "http://127.0.0.1:5001/")
 
-class TestServer:
-    def __init__(self):
-        self.run_args = [
-            'gunicorn',
-            'ddbapi.app.app:app',
-            '-b', '127.0.0.1:5001',
-            '-k', 'uvicorn.workers.UvicornWorker',
-        ]
-        self.proc = None
 
-    def init_proc(self):
-        self.proc = Popen(self.run_args)
+@pytest.fixture(scope="session")
+def mongoclient_mongodb():
+    mongoclient = pymongo.MongoClient(mongo_uri)
+    db = mongoclient[mongo_db]
 
-    def stop_proc(self):
-        self.proc.terminate()
+    yield mongoclient, db
 
-@pytest.fixture(scope="session", autouse=True)
-def server(request):
-    test_server = TestServer()
-    print('Starting server')
-    test_server.init_proc()
-    time.sleep(5)
 
-    def teardown():
-        print('Tearing down server')
-        test_server.stop_proc()
-    
-    request.addfinalizer(teardown)
+@pytest.fixture(scope="session")
+def ddbapi_server_url(mongoclient_mongodb):
+    # mongoclient, mongodb = mongoclient, mongodb
+    with run_dummy_server(
+            ddbapi_test_url, mongo_uri, mongo_db) as server_url:
+        yield server_url
+
+
+@pytest.fixture(scope="session")
+def ddbapi_endpoint_url(ddbapi_server_url):
+    yield posixpath.join(ddbapi_server_url, "api")
+
 
 @pytest.fixture(scope="function")
-def mongo_delete_acq_after(request):
-    acquisitions = db.acquisitions
+def acquisitions(mongoclient_mongodb):
+    mongoclient, mongodb = mongoclient_mongodb
+    yield mongodb.acquisitions
 
+
+@pytest.fixture(scope="function")
+def good_acquisitions():
+    return copy.deepcopy(acquisition_data.acq_good_doc)
+
+
+@contextlib.contextmanager
+def databased_collection(collection, items, drop_after=True):
+    collection.insert_many(copy.deepcopy(items))
+    yield
+    if drop_after:
+        collection.drop()
+
+
+@pytest.fixture(scope="function")
+def databased_good_acquisitions(acquisitions, good_acquisitions):
+    with databased_collection(acquisitions, good_acquisitions):
+        yield copy.deepcopy(good_acquisitions)
+
+
+@pytest.fixture(scope="function")
+def mongo_delete_acq_after(request, acquisitions):
     def teardown():
         acquisitions.drop()
-    
+
     request.addfinalizer(teardown)
 
+
 @pytest.fixture(scope="function")
-def mongo_insert_delete_acq(request):
-    acquisitions = db.acquisitions
+def mongo_insert_delete_acq(request, acquisitions):
+    acquisitions = acquisitions
     acquisitions.insert_many(acquisition_data.acq_good_doc)
 
     def teardown():
         acquisitions.drop()
-    
+
     request.addfinalizer(teardown)
 
-@pytest.fixture()
-def good_acquisitions():
-    return acquisition_data.acq_good_doc
 
 @pytest.fixture()
 def bad_acquisitions():
     return acquisition_data.acq_bad_doc
+
 
 @pytest.fixture()
 def specimen_id():
